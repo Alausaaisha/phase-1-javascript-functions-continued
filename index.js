dd4497077e7b7cbeedee1824d6ddb3ab1e3018b2
Alausa Aishat <qtaisha.ao@gmail.com>
2022-06-12 23:40:15 +0100

    Javascript functions practices

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(toDo = "roller-skate") {
+    return (`This Saturday, I want to ${toDo}!`);
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+const mondayWork = function (todo = "go to the office") {
+
+return (`This Monday, I will ${todo}.`);
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(wrap = "*") {
+    return function (Iam = "special") {
+        return `You are ${wrap}${Iam}${wrap}!`;
+    }
+}
+wrapAdjective();
+wrapAdjective("||")("a dedicated programmer");
